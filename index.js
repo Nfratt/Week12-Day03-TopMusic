18ecee81375432c5a4b03f2dc1a1a60e85ec4c61
Nick Frattaroli <Nafratt@gmail.com>
2019-10-22 21:26:16 -0400

    updaing JS

---
+++ index.js
@@ -0,0 +1,104 @@
+/* eslint-disable require-jsdoc */
+/**
+ * @see {@link https://www.npmjs.com/package/mysql}
+ */
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: 'root',
+  // Your password
+  password: 'Mistymisty@2021',
+  database: 'music_rankings_db',
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  doCRUD();
+});
+/**
+ * Called by callback function upon successful establishment of DB connection.
+ * @see {@link https://www.npmjs.com/package/mysql#performing-queries}
+ */
+function doCRUD() {
+  command(userSelect);
+}
+const userSelect = process.argv[2];
+const userInput = process.argv.slice(3).join(' ');
+function command(userSelect) {
+  switch (userSelect) {
+    case 'songs-by-artist':
+      readSong();
+      break;
+    case 'top-artists':
+      topArtists();
+      break;
+    case 'top-ten':
+      topTen();
+      break;
+    case 'song-data':
+      songData();
+      break;
+  }
+}
+/**
+ * Reads all products from the DB
+ */
+function readSong() {
+  console.log('Selecting all songs by artist...\n');
+  connection.query('SELECT song, year FROM songs WHERE ? ORDER BY year DESC',
+      [
+        {
+          artist: userInput,
+        },
+      ],
+      function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+      });
+}
+function topArtists() {
+  console.log('Selecting all top artists..\n');
+  connection.query('SELECT artist, count(*) as Count FROM songs GROUP BY artist HAVING count(*)>1 ORDER BY Count DESC',
+      function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+      });
+}
+function topTen() {
+  console.log('Selecting top ten artists..\n');
+  connection.query('SELECT song, artist, raw_total as Rating, year FROM songs ORDER BY Rating desc LIMIT 10',
+      function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+      });
+}
+// eslint-disable-next-line require-jsdoc
+function songData() {
+  console.log('Selecting song data..\n');
+  connection.query('SELECT * FROM songs WHERE ?',
+      [
+        {
+          song: userInput,
+        },
+      ],
+      function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+      });
+}
+Collapse;
+
+
+
+
--- schema.sql
+++ schema.sql
@@ -15,6 +15,30 @@ CREATE TABLE songs(
   raw_row INT NULL
 
 );
+-- spec year
 SELECT * from songs 
-WHERE year = '2010'
-;
+ WHERE year = '2004';
+--  spec artist
+Select song, artist, year from songs
+ where artist='Paramore'
+ order by year asc ;
+ 
+--  top 10
+ select * from songs
+ where raw_total ='35';
+-- top 10 try 2
+SELECT * FROM `songs`
+order by raw_total desc
+LIMIT 10;
+-- appears more than once 
+SELECT artist, COUNT(*) AS 'count'
+FROM  songs
+GROUP BY artist
+HAVING COUNT(*)>1
+ORDER BY count(*) DESC;
+
+-- spec song
+SELECT * 
+FROM `songs` 
+WHERE song  ="stan"
+
